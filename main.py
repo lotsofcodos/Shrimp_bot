aea862b75e01f7ee0e1684d6f1a01d57ace942de
Waviest <31796727+WaviestBalloon@users.noreply.github.com>
2021-05-18 00:54:30 +0100

    Fixed grammar mistake

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ bot_token = os.getenv("TOKEN")
 bot = BotFactory('cog_extensions', ('mini_games', 'admin', 'game_cogs'))
 # these are extensions which you can find in cog_extensions... you can remove and add these from the bot to make it do different stuff
 bot.run(bot_token)
-#bot.run runs rhe bot itself
+#bot.run runs the bot itself
 #while for admin is in arcade
   #we wanna use that for .help command so assign to up state cognate and disord commands extension (commands.py)
