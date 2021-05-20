8b78ea841ba6d84012da718c2397cc8db32e6d20
lotsofcodos <>
2021-05-20 18:33:28 +0000

    removed flask hack

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from game_bot import BotFactory, keep_alive
 
-keep_alive()
+#keep_alive()
 bot_token = os.getenv("TOKEN") 
 bot = BotFactory('cog_extensions', ('mini_games', 'admin', 'game_cogs'))
 # these are extensions which you can find in cog_extensions... you can remove and add these from the bot to make it do different stuff
